11d8452694a81dc199e774ec3fc2bd5d67ddc779
amirhanna2001 <amirhanna111200@gmail.com>
2023-04-22 23:32:44 +0200

    DELETE Villa End Point

--- MagicVilla/Controllers/VillaAPIController.cs
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -1,5 +1,7 @@
 ﻿using MagicVilla.Data;
 using MagicVilla.Dtos;
+using MagicVilla.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -27,6 +29,44 @@ namespace MagicVilla.Controllers
                 return NotFound($"No Villa With ID = {id}");
 
             return Ok( villa );
+        }
+
+        [HttpPost]
+        public ActionResult CreateVilla([FromBody] VillaDto villa)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(villa == null)
+                return BadRequest(villa);
+
+            villa.ID = VillaStore.villaList.Count()+1;
+
+            Villa villaToInsert = new()
+            {
+                ID = villa.ID,
+                Name = villa.Name,
+                CreatedOn = DateTime.Now,
+            };
+
+            //VillaStore.villaList.Add(villaToInsert);
+            VillaStore.villaList.Add(villa);
+            return Ok(villaToInsert);
+        }
+
+        [HttpDelete("id")]
+        public ActionResult DeleteVilla(int id)
+        {
+            if (id == null || id == 0)
+                return BadRequest($"Enter a Valid ID");
+
+            VillaDto villa = VillaStore.villaList.FirstOrDefault(v => v.ID == id);
+
+            if (villa == null)
+                return NotFound($"No Villa With ID {id}");
+
+            VillaStore.villaList.Remove(villa); 
+            return Ok(villa);
         }
     }
 
--- MagicVilla/Dtos/VillaDto.cs
+++ MagicVilla/Dtos/VillaDto.cs
@@ -1,8 +1,11 @@
-﻿namespace MagicVilla.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.Dtos
 {
     public class VillaDto
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
