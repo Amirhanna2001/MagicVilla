b70bc7ba3baebd27ea633b82a77e04473104755d
amirhanna2001 <amirhanna111200@gmail.com>
2023-04-22 15:04:31 +0200

    Create DTOS Folder & Get All Villas End Point

--- MagicVilla/Controllers/VillaAPIController.cs
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla.Controllers
@@ -7,5 +7,15 @@ namespace MagicVilla.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        public VillaAPIController() { }
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto>() { 
+                new VillaDto{ID=1,Name = "Villa 1"},
+                new VillaDto{ID=2,Name = "Villa 2"},
+                
+            };
+        }
     }
 }
---
+++ MagicVilla/Dtos/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla.Dtos
+{
+    public class VillaDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
